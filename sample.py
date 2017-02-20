a0e3d9b92a8eef0be10c9ea4a72dab72f3e191a3
Chiaoy <qrslrhko@gmail.com>
2017-02-20 00:09:38 -0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -19,34 +19,5 @@ import sklearn.linear_model
 
 	for i in range(0, num_epochs):
 
-		for j in range(0,10):
-			ans.backward(set_train_x[j],set_train_y[j],learning_rate,momentum,reg)
-		# evaluate test sample
-		many = ans.testing(test_x)
-		count_accuracy = 0			
-		count_accuracy = np.sum(many == test_y.T)
-		test_acc_rate = count_accuracy / 2000
-		test_accuracy = test_acc_rate
-		test_loss = len(test_y.T)- test_accuracy
-
-		# evaluate training sample
-		many = ans.testing(train_x)
-		count_accuracy = 0			
-		count_accuracy = np.sum(many == train_y.T)
-		train_acc_rate = count_accuracy / 10000
-		train_accuracy = train_acc_rate
-		train_loss = len(train_y.T)- train_accuracy
-
-		print()
-		print('    Train Loss: {:.3f}    Train Acc.: {:.2f}%'.format(
-			train_loss,
-			100. * train_accuracy,
-		))
-		print('    Test Loss:  {:.3f}    Test Acc.:  {:.2f}%'.format(
-			test_loss,
-			100. * test_accuracy,
-		))
-		
-		
 		
 		
