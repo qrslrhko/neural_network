5adcd1b82337972fe17f422cb5bcd37ce209d9ab
Chiaoy <qrslrhko@gmail.com>
2017-02-19 23:35:00 -0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -16,22 +16,18 @@ import sklearn.linear_model
 # 
 
 
-for(inputlayer, outputlater) in zip(self.int_hidded_out[:-1],self.int_hidded_out[1:]):
-	self.weight.append(np.random.normal(0,scale = 0.001,size=(inputlayer,outputlater) ) )
-		self.prev_delta_weight.append(np.zeros((inputlayer,outputlater)))
-		
-		
-current_grad_w2 = np.dot(self.Z1.T,delta3 )
-grad_w2 = learning_rate *current_grad_w2 + momentum * self.prev_delta_weight[1]
+train_x = data['train_data']
+train_y = data['train_labels']
+test_x = data['test_data']
+test_y = data['test_labels']
+	
+num_examples, input_dims = train_x.shape
+num_tests, test_dims = test_x.shape
+input_size = train_x.shape[1]
 
-delta2 = delta3.dot(self.weight[1].T) * acti_fun_R.backward(self.Z1)
-current_grad_w1 = np.dot(input_value.T, delta2)
-grad_w1 = learning_rate * current_grad_w1 + momentum *self.prev_delta_weight[0]
+para_list = (input_size,num_hidden,output_size)
+ans = LinearTransform(para_list,num_hidden)
 
-#update previous delta_weights
-self.prev_delta_weight[0] = grad_w1
-self.prev_delta_weight[1] = grad_w2
-
-#update weights 
-self.weight[1] -=  grad_w2
-self.weight[0] -=  grad_w1
+## several processes ............ ## 
+##................................##
+ans.backward(set_train_x[j],set_train_y[j],learning_rate,momentum,reg)
